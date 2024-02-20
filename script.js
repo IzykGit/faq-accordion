5feea6ec5dbfc664e4adc29450b220f61b039023
Izyk <lance_hemphill1234@yahoo.com>
2024-02-20 02:39:51 -0500

    completed

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+const questions = document.querySelectorAll('#question')
+
+const answers = document.querySelectorAll('#answer')
+
+const images = document.querySelectorAll('#questionImage')
+
+
+const plusImage = './assets/images/icon-plus.svg';
+const minusImage = './assets/images/icon-minus.svg';
+
+answers.forEach(answer => {
+    if(!answer.classList.contains('answer-close')) {
+        answer.classList.add('answer-close')
+    }
+})
+
+images.forEach(image => {
+    if(image.src !== plusImage) {
+        image.src = plusImage
+    }
+})
+
+questions.forEach((question, index) => {
+    question.addEventListener("click", () => {
+        answers[index].classList.toggle('answer-close')
+        
+
+        if (images[index].src.contains(plusImage)) {
+            images[index].src = minusImage;
+        } else {
+            images[index].src = plusImage;
+        }
+    })
+})
